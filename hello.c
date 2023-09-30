61f128fee2ded55b785a18b1204732c2e9d6dc3c
dev0405 <146557622+dev0405@users.noreply.github.com>
2023-09-30 20:06:44 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main(){
+    printf("Hello World\n");
+    int c=5;
+    if(c>3)
+        printf("yes greater than 3");
+    else
+        printf("no");
+    return 0;
+}
